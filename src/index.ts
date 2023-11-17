d88877d7563d59a758347983d38e584ffc495c86
Albert Araque <albert.araque.casaus@gmail.com>
2023-11-17 17:14:50 +0100

    Edited tsconfig and index

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Express, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (_req, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
